e171debcd1a14955a4f415aaa443c019f377910e
Pavel <pavelgren105@outlook.com>
2022-10-21 00:02:57 +0300

    0.0.1.1 (SOW added)

---
+++ src/components/Category.tsx
@@ -0,0 +1,11 @@
+import { singleCategory, singleProduct } from "../models"
+import {Product} from "../components/Product";
+import {NavPanel} from "../components/NavPanel";
+import {DropdownMenu} from "../components/Dropdown";
+
+export function Category() {
+    return (
+    <div className="mainWindow">
+       
+    </div>
+)}
---
+++ src/components/MainWindow.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import { singleCategory, singleProduct } from "../models"
+import {Product} from "../components/Product";
+import {NavPanel} from "../components/NavPanel";
+
+export function MainWindow() {
+    return (
+        <div className='Category'>
+
+        </div>
+)}
